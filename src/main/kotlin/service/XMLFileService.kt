a5cb936f77f6b971b642b42d2a81b27b902c6f20
kudrix <max51kud@gmail.com>
2024-11-08 21:39:17 +0300

    done csv reader todo add xml reader

--- src/main/kotlin/service/XMLFileService.kt
+++ src/main/kotlin/service/XMLFileService.kt
@@ -1,9 +1,32 @@
 package service
 
+import model.Address
 import java.io.File
+import kotlin.system.measureTimeMillis
 
-class XMLFileService(private val path: String): FileServiceInterface{
+class XMLFileService(private val path: String): FileServiceInterface {
     override fun open(): Triple<MutableMap<String, Int>, MutableMap<Int, Int>, Long> {
-        TODO("Not yet implemented")
+        val lines = File(path).readLines()
+
+        val addresses = mutableListOf<Address>()
+        val duplicates = mutableMapOf<String, Int>()
+        val floorCount = mutableMapOf<Int, Int>()
+
+        val timeTaken = measureTimeMillis {
+            lines.forEach { line ->
+                val regex = """<item city="([^"]+)" street="([^"]+)" house="(\d+)" floor="(\d+)" />""".toRegex()
+                val matchResult = regex.find(line)
+                if (matchResult != null) {
+                    val (city, street, house, floor) = matchResult.destructured
+                    val address = Address(city, street, house.toInt(), floor.toInt())
+                    addresses.add(address)
+                    duplicates[address.toString()] = duplicates.getOrDefault(address.toString(), 0) + 1
+                    floorCount[address.floor] = floorCount.getOrDefault(address.floor, 0) + 1
+                    addresses.add(Address(city, street, house.toInt(), floor.toInt()))
+                }
+            }
+        }
+
+        return Triple(duplicates, floorCount, timeTaken)
     }
 }
